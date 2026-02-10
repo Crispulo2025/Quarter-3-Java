7ed56fa914782a877304c5a310ccc6ea3be381a1
Crispulo2025 <marpalace1@gmail.com>
2026-02-10 15:27:01 +0800

    Add files via upload

---
+++ ReverseListExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ReverseListExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        Collections.reverse(numbers);
+
+        System.out.println(numbers);
+    }
+}

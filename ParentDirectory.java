3cb73eb0b56bb3c40174b6dd761e44b6cae61cce
Crispulo2025 <marpalace1@gmail.com>
2026-02-15 11:26:58 +0800

    Add files via upload

---
+++ ParentDirectory.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ParentDirectory {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter file path: ");
+        String path = scanner.nextLine();
+
+        File file = new File(path);
+
+        String parent = file.getParent();
+
+        if (parent != null) {
+            System.out.println("Parent Directory: " + parent);
+        } else {
+            System.out.println("No parent directory found.");
+        }
+
+        scanner.close();
+    }
+}
